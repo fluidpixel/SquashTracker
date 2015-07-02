993ca9ff9785ee6c88ce93b22ebe4b1c7709b083
Stuart Varrall <git@fpstudios.com>
2015-07-02 15:50:33 +0100

    (new) swiftcharts

---
+++ SquashTracker/SwiftCharts/ChartPoint/ChartPointBubble.swift
@@ -0,0 +1,26 @@
+//
+//  ChartPointBubble.swift
+//  Examples
+//
+//  Created by ischuetz on 17/05/15.
+//  Copyright (c) 2015 ivanschuetz. All rights reserved.
+//
+
+import UIKit
+
+public class ChartPointBubble: ChartPoint {
+    public let diameterScalar: CGFloat
+    public let bgColor: UIColor
+    public let borderColor: UIColor
+    
+    public init(x: ChartAxisValue, y: ChartAxisValue, diameterScalar: CGFloat, bgColor: UIColor, borderColor: UIColor = UIColor.blackColor()) {
+        self.diameterScalar = diameterScalar
+        self.bgColor = bgColor
+        self.borderColor = borderColor
+        super.init(x: x, y: y)
+    }
+
+    required public init(x: ChartAxisValue, y: ChartAxisValue) {
+        fatalError("init(x:y:) has not been implemented")
+    }
+}
